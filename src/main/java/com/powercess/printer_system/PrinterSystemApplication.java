ea08936ff9339223d40242ed2fa7ce7285dbd185
CN059 <cn059@outlook.com>
2026-03-17 22:55:05 +0800

    init: 初始化项目

---
+++ src/main/java/com/powercess/printer_system/PrinterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.powercess.printer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrinterSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powercess/printer_system/PrinterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powercess.printer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
